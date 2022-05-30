e9fffb39b5fb13caecc9f15a77b3876c3b4a262f
Angus Muller <>
2022-05-30 14:41:34 +0100

    Networking, repository and models structure

---
+++ F1 Driver Standings URLSession/ViewModel/DriverStandingsViewModel.swift
@@ -0,0 +1,56 @@
+//
+//  DriverStandingsViewModel.swift
+//  F1 Driver Standings URLSession
+//
+//  Created by Angus Muller on 30/05/2022.
+//
+
+import Foundation
+
+class DriverStandingsViewModel {
+    private var standing: StandingsList?
+    private var driverStanding: [DriverStanding]? {
+        standing?.driverStandings.sorted { $0.position > $1.position }
+    }
+    var f1Repository: F1Repository
+    
+    init(f1Repository: F1Repository) {
+        self.f1Repository = f1Repository
+    }
+    
+    /// Pulls the latest set of rockets
+    func refresh(completion: @escaping (_ errorMessage: String?) -> ()) {
+        f1Repository.fetchCurrentDriverStandings { result in
+            switch result {
+            case .failure(let error):
+                completion(error.errorDescription)
+            case .success(let standing):
+                self.standing = standing
+                completion(nil)
+            }
+        }
+    }
+}
+
+// MARK: - Standing data
+
+extension DriverStandingsViewModel {
+    func allDriverStandings() -> [DriverStanding]? {
+        return driverStanding
+    }
+    
+    func driverStanding(at index: Int) -> DriverStanding? {
+        guard index < driverStandingCount() else {
+            return nil
+        }
+        return allDriverStandings()?[index]
+    }
+    
+    func driverStandingCount() -> Int {
+        return driverStanding?.count ?? 0
+    }
+    
+    func seasonName() -> String? {
+        return standing?.season
+    }
+}
